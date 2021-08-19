67cf99a9cc85a01e1824a020c269e6e31c3a7933
Serhii <sergei181989@gmail.com>
2021-08-19 18:01:26 +0300

    :sparkles:  added Router link & added components 'Favorites', 'Login', 'Room'.

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Favorites from '../Favorites';
+import Login from '../Login';
 import Main from '../Main';
+import Room from '../Room';
 
 function App() {
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
+        <Route path="/offer" exact>
+          <Room />
+        </Route>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
